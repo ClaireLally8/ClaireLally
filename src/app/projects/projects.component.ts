bf825d37f4bd69ba4f08871e197404a4501a10c3
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2021-03-23 13:49:16 +0000

    Created my projects section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { projects } from '../projects';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProjectsComponent {
+  projects=projects;
 }
