60e71f5a943db30feb16d662f476bb23489b1209
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2021-03-23 14:06:46 +0000

    refactored

--- src/app/main-section/main-section.component.ts
+++ src/app/main-section/main-section.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-section',
-  templateUrl: './main-section.component.html',
-  styleUrls: ['./main-section.component.css']
+  templateUrl: './main-section.component.html'
 })
 export class MainSectionComponent implements OnInit {
 
